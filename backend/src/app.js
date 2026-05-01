308a72f4c543a55ea70275985ae06c31c2144376
Driart Gajtani <gajtanidriart@gmail.com>
2026-05-01 17:19:34 +0200

    Change CORS origin to true

--- backend/src/app.js
+++ backend/src/app.js
@@ -38,7 +38,7 @@ app.use(helmet());
 // CORS
 app.use(
   cors({
-    origin: env.get('CLIENT_URL'),
+    origin: true,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
